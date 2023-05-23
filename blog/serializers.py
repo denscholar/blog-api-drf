128b5cdb6fa8dd324161257ac7b8735dbd9e087b
Dennis Akagha <dennis.akagha@afripointgroup.com>
2023-05-23 01:17:29 +0100

    learnt about validators

--- blog/serializers.py
+++ blog/serializers.py
@@ -3,7 +3,23 @@ from .models import Blog
 
 
 class BlogSerializers(serializers.ModelSerializer):
+    # validator
+    name = serializers.StringField(required=True)
+    description = serializers.StringField(required=True)
+
     class Meta:
         model = Blog
-        fields = ('name', 'description', 'is_public', 'post_date', 'slug')
-        read_only_fields = ('post_date', 'slug')
+        fields = ("name", "description", "is_public", "post_date", "slug")
+        read_only_fields = ("post_date", "slug")
+
+    # fields level validation
+    def validate_name(self, value):
+        if len(value) < 4:
+            raise serializers.ValidationError("blog title iss very short")
+
+    # object level vaalidation
+    def validate(self, data):
+        if data["name"] == data["description"]:
+            raise serializers.ValidationError("name and description can't be same")
+        else:
+            return data
